bfae6e9cd0a7414a81babe0f90a76f080ac17dae
Andrew <100212162+demoh77@users.noreply.github.com>
2024-12-11 03:42:24 +0000

    Update auth.py

--- ca2/auth.py
+++ ca2/auth.py
@@ -2,8 +2,6 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from passlib.hash import pbkdf2_sha256
-import logging
-from logging.handlers import RotatingFileHandler
 from . import db   # means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 from flask_limiter import Limiter
